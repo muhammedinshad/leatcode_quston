b02a3200e7a9f050ac3864a6594b33dc8f5df9b8
Muhammed Inshad <muhammedinshad30@gmail.com>
2026-02-02 21:15:58 +0530

    Time: 0 ms (100%), Space: 12.5 MB (14.69%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        """
+        :type hours: List[int]
+        :type target: int
+        :rtype: int
+        """
+        a = list(filter(lambda x: x >= target,hours))
+        return len(a)
