4bdd0ff3cfcad1a05dd28c3ce82e878433c3238d
Muhammed Inshad <muhammedinshad30@gmail.com>
2026-07-11 23:20:41 -0700

    Time: 19 ms (65.51%), Space: 12.4 MB (85.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,15 +5,19 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        res = False
-        if len(t) == len(s):
-            for i in s:
-                if s.count(i) == t.count(i):
-                    res=True
-                else:
-                    res = False
-                    break
-        return res
+        if len(s) != len(t):
+            return False
 
-                
+        cnt = [0] * 26
+
+        for c in s:
+            cnt[ord(c) - ord('a')] += 1
+
+        for c in t:
+            idx = ord(c) - ord('a')
+            cnt[idx] -= 1
+            if cnt[idx] < 0:
+                return False
+
+        return True
         
