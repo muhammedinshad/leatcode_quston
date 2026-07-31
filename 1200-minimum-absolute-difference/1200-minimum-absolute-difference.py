34570f34d81084a624a07fab085946ab82070202
Muhammed Inshad <muhammedinshad30@gmail.com>
2026-07-31 22:16:39 +0530

    Time: 72 ms (68.94%), Space: 21.7 MB (47.44%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minimumAbsDifference(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[List[int]]
+        """
+        arr.sort()
+        min_diff = float('inf')
+        result = []
+
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i - 1]
+
+            if diff < min_diff:
+                min_diff = diff
+                result = [[arr[i - 1], arr[i]]]
+            elif diff == min_diff:
+                result.append([arr[i - 1], arr[i]])
+
+        return result
+        
