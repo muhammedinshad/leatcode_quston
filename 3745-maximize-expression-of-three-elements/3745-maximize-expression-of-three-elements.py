74b77e763098b4d32d983b9ad4f72f0c5e45fd35
Muhammed Inshad <muhammedinshad30@gmail.com>
2026-08-01 09:55:26 +0530

    Time: 0 ms (100%), Space: 12.2 MB (100%) - LeetHub

---
+++ 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maximizeExpressionOfThree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = sorted(nums)
+        res = n[-1]+n[-2]-n[0]
+        return res
+        
